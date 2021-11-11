6d36c54072c5b01daaf9cba13b2e22a935456311
ram1071999 <ramvashishtha2017@gmail.com>
2021-11-11 09:44:46 +0530

    Initial commit

---
+++ tv.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping</title>
+
+    <!-- owl carousel css file cdn link  -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="http://localhost/ecommerce_website/css/tv.css">
+
+</head>
+<body>
+
+<!-- header section starts  -->
+
+<header>
+
+<div class="header-1">
+
+    <a href="#" class="logo"> <i class="fas fa-shopping-bag"></i>shopping </a>
+
+    <div class="form-container">
+        <form action="" class="search">
+            <input type="search" placeholder="search products" id="search" /> 
+            <label for="search" class="fas fa-search"></label>
+        </form>
+
+    </div>
+
+</div>
+
+<div class="header-2">
+
+    <div id="menu" class="fas fa-bars"></div>
+
+    <nav class="navbar">
+        <ul>
+            <li><a class="active" href="#home">home</a></li>
+            <li><a href="#arrival">arrival</a></li>
+            <li><a href="#featured">featured</a></li>
+            <li><a href="#gallery">gallery</a></li>
+            <li><a href="#deal">deal</a></li>
+        </ul>
+    </nav>
+
+    <div class="icons">
+        <a href="#" class="fas fa-heart"></a>
+        <a href="#" class="fas fa-shopping-cart"></a>
+        <a href="#" class="fas fa-user"></a>
+    </div>
+
+</div>
+
+</header>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-2" id="border">
+            <div class="container py-5">
+
+               <h3>Laptop</h3>
+             
+              <hr>
+              <br>
+              <h3>Brand</h3>
+              <hr>
+              
+              <div class="Brand">
+                 <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">HP</span><br>
+                  <input type="checkbox" name="Lenovo">&nbsp;&nbsp;<span class="br">Lenovo</span><br>
+                   <input type="checkbox" name="Dell">&nbsp;&nbsp;<span class="br">Dell</span><br>
+                    <input type="checkbox" name="ASUS">&nbsp;&nbsp;<span class="br">ASUS</span><br> 
+                     <input type="checkbox" name="AVITA">&nbsp;&nbsp;<span class="br">AVITA</span> <br>
+                      <input type="checkbox" name="Acer">&nbsp;&nbsp;<span class="br">Acer</span><br>
+                      <input type="checkbox" name="RDP">&nbsp;&nbsp;<span class="br">RDP</span>
+                
+              </div>
+              <br>
+              <h3 class="py-3"> Customer Rating</h3>
+              <hr>
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                       <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                    </div><br>
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                       <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                      <i class="far fa-star"></i>
+                    </div><br>
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                      <i class="far fa-star"></i>
+                      <i class="far fa-star"></i>
+                    </div><br>
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                      <i class="far fa-star"></i>
+                      <i class="far fa-star"></i>
+                      <i class="far fa-star"></i>
+                    </div><br>
+
+                    <h3 class="py-3">Discount %</h3><hr><br>
+                     <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">80-90</span><br>
+                      <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">70-80</span><br>
+                       <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">60-70</span><br>
+                        <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">50-60</span><br>
+                         <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">40-50</span><br>
+                          <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">30-40</span><br><br>
+
+              <h3 class="py-3">PROCESSOR</h3><hr><br>
+              <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">Core i5</span><br>
+                      <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">Core i3</span><br>
+                       <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">Core i7</span><br>
+                        <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">Ryzen 7 Quad Core</span><br>
+                         <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">Ryzen 5 Quad Core</span><br>
+                          <input type="checkbox" name="hp">&nbsp;&nbsp;<span class="br">Core i9</span>
+
+            </div>
+        </div>
+        <div class="col-10" id="lap">
+            <ul class="nav">
+                <li class="nav-link">
+                    <a class="nav-link" href="https://www.flipkart.com/search?q=laptop&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="img/lap1.jpg" class="img">
+                        </div>
+                        <div class="col-md-9" >
+                            <div class="container">
+                               <p class="text-center"> <h3>Lenovo IdeaPad Slim 3 2021 Intel Core i3 11th Gen 14 inches FHD, LED IPS T&L Business, Laptop (8GB/256GB SSD/Windows 10/MS Office/Backlit/Fingerprint Reader/Arctic Grey/1.41kg)</h3><br>
+                    
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                    </div><br>
+                       <h3 class="text-info"> <i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;41,900</h3><br>
+                       </p>
+                 </div>
+
+                </div>
+                        
+                    </div></a>
+                </li>
+                 <li class="nav-link">
+                     <a class="nav-link" href="https://www.flipkart.com/search?q=laptop&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off">
+                     <div class="row">
+                        <div class="col-md-3">
+                            <img src="img/lap2.webp" class="img">
+                        </div>
+                        <div class="col-md-9">
+                           <p class="text-center"> <h3>ASUS VivoBook 14 (2020) Intel Quad Core Pentium Silver N5030, 14-inch (35.56 cms) FHD Thin and Light Laptop (4GB RAM/1TB HDD/Windows 10/Integrated Graphics/Transparent Silver/1.5 Kg), X415MA-EK101T</h3><br>
+                    
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                    </div><br>
+                       <h3 class="text-info"> <i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;26,990</h3><br>
+                       </p>
+
+
+                </div>
+                        
+                    </div>
+                </a>
+                 </li>
+                  <li class="nav-link">
+                     <a class="nav-link" href="https://www.flipkart.com/search?q=laptop&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off">
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="img/lap3.webp" class="img">
+                        </div>
+                        <div class="col-md-9">
+                                <p class="text-center"> <h3>Dell XPS 9305 13.3 FHD Display Thin & Light Laptop (i5-1135G7 / 16GB / 512GB SSD / Integrated Graphics / Win10 + MSO / Backlit KB + FPR / Silver) D560053WIN9S</h3><br>
+                    
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                    </div><br>
+                       <h3 class="text-info"> <i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;1,17,262</h3><br>
+                       </p>
+
+
+                </div>
+                        
+                    </div>
+                       </a>
+                </li>
+                   <li class="nav-item">
+                     <a class="nav-link" href="#">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="img/lap4.webp" class="img">
+                        </div>
+                        <div class="col-md-9">
+                                 <p class="text-center"><h3>
+                       Lenovo Ideapad Slim 3 10th Gen Intel Core i3 15.6" (39.62cm) FHD Thin & Light Laptop (4GB/256 GB SSD/UHD Graphics/Windows 10/MS Office/2 Year Warranty/Platinum Grey/1.7Kg)</h3><br>
+                    
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                    </div><br>
+                       <h3 class="text-info"> <i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;33,990</h3><br>
+                       </p>
+
+
+                </div>
+                        
+                    </div></a></li>
+                    <li>
+                        <a class="nav-link" href="#">
+                            
+                        <div class="row">
+                        <div class="col-md-3">
+                            <img src="img/lap5.webp" class="img">
+                        </div>
+                        <div class="col-md-9">
+                                 <p class="text-center"> <h3>ASUS VivoBook 15 (2020), 39.6 cm HD, Dual Core Intel Celeron N4020, Thin and Light Laptop (4GB RAM/256GB SSD/Integrated Graphics/Windows 10 Home/Transparent Silver/1.8 Kg), X515MA-</h3><br>
+                    
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                    </div><br>
+                       <h3 class="text-info"> <i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;26,990</h3><br>
+                       </p>
+
+
+                </div>
+                        
+                    </div>
+                    </a>
+
+                </li>
+                     <li class="nav-item">
+                        <a href="#" class="nav-link">
+                        <div class="row">
+                        <div class="col-md-3">
+                            <img src="img/lap6.webp" class="img"s>
+                        </div>
+                        <div class="col-md-9">
+                                <p class="text-center"> <h3>
+                                HP Chromebook 14-inch (35.56 cms) Thin & Light Touchscreen Laptop (Celeron N4020/4GB/64GB eMMC + 256GB Expandable Storage/Chrome OS/1.46 kgs Light/ Mineral Silver), 14a-na0003TU</h3><br>
+                    
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star-half"></i>
+                    </div><br>
+                   <h3 class="text-info"> <i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;27,490</h3><br>
+                       </p>
+
+                </div>
+                        
+                    </div></li>
+            </ul>
+            
+        </div>
+    </div>
+    
+</div>
+
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <a href="#" class="logo"> <i class="fas fa-shopping-bag"></i>  shopping </a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique expedita molestiae distinctio facere beatae velit, maiores ullam molestias necessitatibus sapiente.</p>
+        </div>
+
+        <div class="box">
+            <h3>links</h3>
+            <a href="#">home</a>
+            <a href="#">arrival</a>
+            <a href="#">featured</a>
+            <a href="#">gallery</a>
+            <a href="#">deal</a>
+        </div>
+
+        <div class="box">
+            <h3>contact us</h3>
+            <p> <i class="fas fa-home"></i>
+                namrata, maharashtra, 
+                india - 400104
+            </p>
+            <p> <i class="fas fa-phone"></i>
+                +910000000
+            </p>
+            <p> <i class="fas fa-globe"></i>
+                xyz@yoursite.com
+            </p>
+        </div>
+
+    </div>
+
+<h1 class="credit"> created by <span>mr. Ram Vashishtha</span> | all rights reserved. </h1>
+
+</section>
+
+</body>
+</html>
